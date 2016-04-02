e739bda7c82893dcf417bbf7bac1014f44728b77
mdgriest <mdgriest@crimson.ua.edu>
2016-04-01 19:32:45 -0500

    Initial Commit

---
+++ sicp2_1_01.scm
@@ -0,0 +1,16 @@
+;{
+    Mitchell Griest
+    The University of Alabama
+    Spring 2016
+    CS 403 Programming Languages
+    Studying for Exam 02
+
+    SICP Quiz 2.1
+
+    Question 1:
+    Suppose the rational constructor make-rat could be passed both integers AND
+    rational numbers. How could the constructor identify the type of its
+    arguments in order to simply its result? Choose the most complete answer.
+
+    http://beastie.cs.ua.edu/proglan/2-1.html
+;}
