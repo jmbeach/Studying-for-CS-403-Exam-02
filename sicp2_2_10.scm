53c389e9b4fa5b78a1cf5f546d2bfdd81d491a9d
mdgriest <mdgriest@crimson.ua.edu>
2016-04-03 18:16:16 -0500

    SICP 2.2 10

---
+++ sicp2_2_10.scm
@@ -0,0 +1,22 @@
+;{
+    Mitchell Griest
+    Jacob Zarobsky
+    The University of Alabama
+    Spring 2016
+    CS 403 Programming Languages
+    Studying for Exam 02
+
+    SICP Quiz 2.2
+
+    Question 10:
+    - Consider the singly-linked list 2→5→3→nil.
+    - Draw a cons cell representation of this list, assuming there is
+    a head and a tail pointer. Use the style of the text.
+    - Use the variable items to point to this list
+
+    I am not quite sure which bit of the text we are supposed to base
+    this on, I have included a drawing of my best guess in "SICP 2.2 10.pdf"
+
+    http://beastie.cs.ua.edu/proglan/2-2.html
+;}
+(include "exprTest.scm")
